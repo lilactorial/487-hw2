849929168cc69c172bb7f77d85f2714fd5263d75
lilactorial <58881645+lilactorial@users.noreply.github.com>
2024-10-10 00:55:59 -0400

    Add files via upload

--- commands.sql
+++ commands.sql
@@ -1,42 +1,53 @@
 -- delete tables to empty database
-DELETE TABLE Reservations;
-DELETE TABLE Prices;
-DELETE TABLE Cars;
+DROP TABLE Reservations;
+DROP TABLE Prices;
+DROP TABLE Cars;
 
 -- make the tables
 
 CREATE TABLE Reservations (
-    resID INTEGER,
-    name VARCHAR(255), 
-    address VARCHAR(255),
+    resID INTEGER NOT NULL AUTO_INCREMENT,
+    firstname VARCHAR(255),
+    lastname VARCHAR(255),
     phone VARCHAR(255),
     cartype VARCHAR(10),
     plate VARCHAR(10),
     orderdate DATETIME,
-    startdate DATE,
-    enddate DATE,
+    startdate VARCHAR(30),
+    enddate VARCHAR(30),
     PRIMARY KEY (resID)
     );
 
-CREATE TABLE Prices (
-    cartype VARCHAR(255),
-    price INTEGER,
-    discountprice INTEGER,
-    PRIMARY KEY(cartype)
-);
-
 CREATE TABLE Cars (
     plate VARCHAR(10),
-    cartype VARCHAR(10),
-    reserved 
+    cartype VARCHAR(10)
     PRIMARY KEY (plate)
 );
 
-ALTER TABLE Reservations
-ADD FOREIGN KEY (cartype) REFERENCES Prices(cartype);
+CREATE TABLE Prices (
+    cartype VARCHAR(10),
+    price FLOAT,
+    discountprice FLOAT,
+    PRIMARY KEY (cartype)
+);
 
 ALTER TABLE Reservations
 ADD FOREIGN KEY (plate) REFERENCES Cars(plate);
+ALTER TABLE Reservations
+ADD FOREIGN KEY (cartype) REFERENCES Prices(cartype);
 
--- Add sample entries to the databases
-INSERT INTO Cars 
+-- Add sample entries to the car database
+INSERT INTO Cars VALUES ('ABC1111', 'sedan');
+INSERT INTO Cars VALUES ('DEF2222', 'sedan');
+INSERT INTO Cars VALUES ('GHI3333', 'suv');
+INSERT INTO Cars VALUES ('JKL4444', 'suv');
+INSERT INTO Cars VALUES ('MNO5555', 'pickup');
+INSERT INTO Cars VALUES ('PQR6666', 'pickup');
+INSERT INTO Cars VALUES ('STU7777', 'van');
+INSERT INTO Cars VALUES ('VWX8888', 'van');
+
+-- Initial prices
+INSERT INTO Prices VALUES ('sedan', 40.0, 32.0);
+INSERT INTO Prices VALUES ('suv', 50.0, 40.0);
+INSERT INTO Prices VALUES ('pickup', 80.0, 64.0);
+INSERT INTO Prices VALUES ('van', 60.0, 48.0);
--- hw2.py
+++ hw2.py
@@ -1,5 +1,7 @@
 import mysql.connector
-import tkinter
+import tkinter as tk
+from tkinter import ttk
+from tkcalendar import Calendar 
 
 mydb = mysql.connector.connect(
   host="localhost",
@@ -10,20 +12,83 @@ mydb = mysql.connector.connect(
 
 mycursor = mydb.cursor()
 
-mycursor.execute("""
-    CREATE TABLE Cars (
-    plate VARCHAR(10),
-    cartype VARCHAR(10),
-    resID INTEGER,
-    PRIMARY KEY (plate)
-);
+root = tk.Tk()
+root.title('Mr. Johnson\'s Car Reserver!')
 
-ALTER TABLE Reservations
-ADD FOREIGN KEY (cartype) REFERENCES Prices(cartype);
+# RESERVE PAGE
+def reservepage():
+    reserve = tk.Toplevel(root)
+    reserve.title('Reservation Form')
+    
+    tk.Label(reserve, text='First Name', height=5,width=15,padx=10,pady=5).grid(row=0)
+    tk.Label(reserve, text='Last Name', height=5,width=15,padx=10,pady=5).grid(row=1)
+    first = tk.Entry(reserve)
+    last = tk.Entry(reserve)
+    first.grid(row=0, column=1)
+    last.grid(row=1, column=1)
+    
+    tk.Label(reserve, text='Phone Number', height=5,width=15,padx=10,pady=5).grid(row=2)
+    phone = tk.Entry(reserve)
+    phone.grid(row=2, column=1)
+    
+    tk.Label(reserve, text='Car Type', height=5,width=15,padx=10,pady=5).grid(row=3)
+    cartype = ttk.Combobox(reserve, values= ['sedan', 'suv', 'pickup', 'van'])
+    cartype.set('sedan')
+    cartype.grid(row=3, column=1)
+    
+    tk.Label(reserve, text='Start Date', height=5,width=15,padx=10,pady=5).grid(row=4)
+    startcal = Calendar(reserve, selectmode= 'day')
+    startcal.grid(row=4, column=1)
+    
+    tk.Label(reserve, text='End Date', height=5,width=15,padx=10,pady=5).grid(row=5)
+    endcal = Calendar(reserve, selectmode= 'day')
+    endcal.grid(row=5, column=1)
+    
+    def popup(txt):
+        popup = tk.Toplevel(reserve)
+        popup.title('Information')
+        tk.Label(popup, text=txt, height=10,width=35,padx=10,pady=5).grid(row=0)
+    
+    
+    def submitRes():
+        daterange = (endcal.selection_get() - startcal.selection_get()).days
+        if daterange < 0:
+            popup('Error: Date range is invalid!')
+        else:
+            price = daterange
+            if daterange >= 7:
+                mycursor.execute("SELECT discountprice FROM Prices where cartype = \'" + str(cartype.get()) + "\'")
+                price *= mycursor.fetchone()[0]
+            else:
+                mycursor.execute("SELECT price FROM Prices where cartype = \'" + str(cartype.get()) + "\'")
+                price *= mycursor.fetchone()[0]
+                
+            popup('Total price will be:  $' + str(price))
+            
+            
+        
+            
+    
+    submit = tk.Button(reserve, text="Submit", command=submitRes, height=5,width=15,padx=10,pady=5)
+    submit.grid(row=6, column=1)
 
-ALTER TABLE Reservations
-ADD FOREIGN KEY (plate) REFERENCES Cars(plate);
 
-ALTER TABLE Cars
-ADD FOREIGN KEY (resID) REFERENCES Reservations(resID);
-""")
+# BROWSE PAGE
+def browsepage():
+    browse = tk.Toplevel(root)
+    browse.title('Browse Reservations')
+    print("JOHNSON!")
+
+
+# LOGIN PAGE
+customerbtn = tk.Button(root, text="Continue as Customer", command=reservepage, height=5,width=15,padx=10,pady=5)
+customerbtn.grid(row=0)
+
+johnsonbtn = tk.Button(root, text="Login as Mr. Johnson", command=browsepage, height=5,width=15,padx=10,pady=5)
+johnsonbtn.grid(row=1)
+
+exitbtn = tk.Button(root, text="Exit", command=root.destroy, height=5,width=15,padx=10,pady=5)
+exitbtn.grid(row=2)
+
+
+root.mainloop()
