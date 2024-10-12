0b588df852e4db4a60b4e7c27cac82cf5de5605b
lilactorial <58881645+lilactorial@users.noreply.github.com>
2024-10-11 22:43:11 -0400

    Add files via upload

--- commands.sql
+++ commands.sql
@@ -11,18 +11,10 @@ CREATE TABLE Reservations (
     lastname VARCHAR(255),
     phone VARCHAR(255),
     cartype VARCHAR(10),
-    plate VARCHAR(10),
-    orderdate DATETIME,
     startdate VARCHAR(30),
     enddate VARCHAR(30),
     PRIMARY KEY (resID)
     );
-
-CREATE TABLE Cars (
-    plate VARCHAR(10),
-    cartype VARCHAR(10)
-    PRIMARY KEY (plate)
-);
 
 CREATE TABLE Prices (
     cartype VARCHAR(10),
@@ -32,19 +24,7 @@ CREATE TABLE Prices (
 );
 
 ALTER TABLE Reservations
-ADD FOREIGN KEY (plate) REFERENCES Cars(plate);
-ALTER TABLE Reservations
 ADD FOREIGN KEY (cartype) REFERENCES Prices(cartype);
-
--- Add sample entries to the car database
-INSERT INTO Cars VALUES ('ABC1111', 'sedan');
-INSERT INTO Cars VALUES ('DEF2222', 'sedan');
-INSERT INTO Cars VALUES ('GHI3333', 'suv');
-INSERT INTO Cars VALUES ('JKL4444', 'suv');
-INSERT INTO Cars VALUES ('MNO5555', 'pickup');
-INSERT INTO Cars VALUES ('PQR6666', 'pickup');
-INSERT INTO Cars VALUES ('STU7777', 'van');
-INSERT INTO Cars VALUES ('VWX8888', 'van');
 
 -- Initial prices
 INSERT INTO Prices VALUES ('sedan', 40.0, 32.0);
--- hw2.py
+++ hw2.py
@@ -1,8 +1,16 @@
+##################################
+#   Lilac Dixon
+#   CMPSC 487W
+#   Section 002   
+#
+##################################
+
 import mysql.connector
 import tkinter as tk
 from tkinter import ttk
 from tkcalendar import Calendar 
 
+# connect localhost database
 mydb = mysql.connector.connect(
   host="localhost",
   user="root",
@@ -12,19 +20,30 @@ mydb = mysql.connector.connect(
 
 mycursor = mydb.cursor()
 
+# root window, when closed everything else will close
 root = tk.Tk()
 root.title('Mr. Johnson\'s Car Reserver!')
 
+# popup window for errors or other informations for the user
+def popupfn(txt):
+        popup = tk.Toplevel(root)
+        popup.title('Popup')
+        tk.Label(popup, text=txt, height=10,width=35,padx=10,pady=5).grid(row=0)
+
 # RESERVE PAGE
+# for customers placing reservations
 def reservepage():
     reserve = tk.Toplevel(root)
     reserve.title('Reservation Form')
     
+    
+    # create buttons and labels and other entry methods
     tk.Label(reserve, text='First Name', height=5,width=15,padx=10,pady=5).grid(row=0)
-    tk.Label(reserve, text='Last Name', height=5,width=15,padx=10,pady=5).grid(row=1)
     first = tk.Entry(reserve)
-    last = tk.Entry(reserve)
     first.grid(row=0, column=1)
+    
+    tk.Label(reserve, text='Last Name', height=5,width=15,padx=10,pady=5).grid(row=1)
+    last = tk.Entry(reserve)
     last.grid(row=1, column=1)
     
     tk.Label(reserve, text='Phone Number', height=5,width=15,padx=10,pady=5).grid(row=2)
@@ -44,47 +63,181 @@ def reservepage():
     endcal = Calendar(reserve, selectmode= 'day')
     endcal.grid(row=5, column=1)
     
-    def popup(txt):
-        popup = tk.Toplevel(reserve)
-        popup.title('Information')
-        tk.Label(popup, text=txt, height=10,width=35,padx=10,pady=5).grid(row=0)
-    
-    
+    # function for submitting reservation to database.
+    # also makes sure reservation is valid.
     def submitRes():
         daterange = (endcal.selection_get() - startcal.selection_get()).days
+        # check for invalid dates
         if daterange < 0:
-            popup('Error: Date range is invalid!')
+            popupfn('Error: Date range is invalid!')
+        #
+        elif (first.get() == "" or last.get() == "" or phone.get() == ""):
+            popupfn('Error: Empty fields! Please fill in all entry fields.')
         else:
-            price = daterange
+            price = daterange + 1
+            
+            # get correct price (discount or full)
             if daterange >= 7:
                 mycursor.execute("SELECT discountprice FROM Prices where cartype = \'" + str(cartype.get()) + "\'")
                 price *= mycursor.fetchone()[0]
             else:
                 mycursor.execute("SELECT price FROM Prices where cartype = \'" + str(cartype.get()) + "\'")
                 price *= mycursor.fetchone()[0]
-                
-            popup('Total price will be:  $' + str(price))
             
+            # put entry into the database
+            sql = "INSERT INTO Reservations (firstname, lastname, phone, cartype, startdate, enddate) VALUES (%s, %s, %s, %s, %s, %s)"
+            val = (first.get(), last.get(), phone.get(), cartype.get(), startcal.get_date(), endcal.get_date())
             
-        
+            mycursor.execute(sql, val)
+            mydb.commit()
             
+            print(mycursor.rowcount, "record inserted.")
+            
+            # show total price and that reservation was placed.
+            popupfn('Reservation placed!\nTotal price will be:  $' + str(price))
+            
+            # close the reservation window
+            reserve.destroy()
     
+    # button to commit the reservation
     submit = tk.Button(reserve, text="Submit", command=submitRes, height=5,width=15,padx=10,pady=5)
-    submit.grid(row=6, column=1)
+    submit.grid(row=6, columnspan=2)
+    
 
+# page for mr johnson to pick between his options
+def johnsonpage():
+    johnson = tk.Toplevel(root)
+    johnson.title('Pick Action')
 
-# BROWSE PAGE
-def browsepage():
-    browse = tk.Toplevel(root)
-    browse.title('Browse Reservations')
-    print("JOHNSON!")
+    # BROWSE PAGE
+    # to look at all the reservations
+    def browsepage():
+        browse = tk.Toplevel(johnson)
+        browse.title('Browse Reservations')
+        browse.geometry('400x400')
+        
+        # make scrollbar to scroll through the entries
+        scrollbar = tk.Scrollbar(browse)
+        scrollbar.pack(side='right', fill='y')
+        
+        # listbox to easily display the entries
+        reservationlist = tk.Listbox(browse, yscrollcommand=scrollbar.set)
+        
+        mycursor.execute("SELECT * FROM Reservations")
+        myresult = mycursor.fetchall()
+        
+        reservationlist.insert(tk.END, "First Name | Last Name | Phone | Car Type | Start Date | End Date")
+        reservationlist.insert(tk.END, "-----------------------------------------------------------------")
+        
+        # print all the reservations in the listbox
+        for x in myresult:
+            string = str(x[1]) + '  |  ' + str(x[2]) + '  |  ' + str(x[3]) + '  |  ' + str(x[4]) + '  |  ' + str(x[5]) + '  |  ' + str(x[6])
+            reservationlist.insert(tk.END, string)
+            
+        reservationlist.pack(side='left', fill='both')
+        reservationlist['width'] = 400
+        scrollbar.config(command=reservationlist.yview)
+    
+    # PAGE TO CHANGE PRICES
+    def changeprice():
+        pricepage = tk.Toplevel(johnson)
+        pricepage.title('Change Prices')
+        
+        # label and entrybox for each price
+        tk.Label(pricepage, text='Sedan Price/Day', height=5,width=20,padx=10,pady=5).grid(row=0, column=0)
+        sedanreg = tk.Entry(pricepage)
+        sedanreg.grid(row=1, column=0)
+        mycursor.execute("SELECT price FROM Prices WHERE cartype = \'sedan\'")
+        sedanreg.insert(0, mycursor.fetchone()[0])
+        
+        tk.Label(pricepage, text='Sedan Discount Price/Day', height=5,width=20,padx=10,pady=5).grid(row=0, column=1)
+        sedandis = tk.Entry(pricepage)
+        sedandis.grid(row=1, column=1)
+        mycursor.execute("SELECT discountprice FROM Prices WHERE cartype = \'sedan\'")
+        sedandis.insert(0, mycursor.fetchone()[0])
+        
+        tk.Label(pricepage, text='SUV Price/Day', height=5,width=20,padx=10,pady=5).grid(row=2, column=0)
+        suvreg = tk.Entry(pricepage)
+        suvreg.grid(row=3, column=0)
+        mycursor.execute("SELECT price FROM Prices WHERE cartype = \'suv\'")
+        suvreg.insert(0, mycursor.fetchone()[0])
+        
+        tk.Label(pricepage, text='SUV Discount Price/Day', height=5,width=20,padx=10,pady=5).grid(row=2, column=1)
+        suvdis = tk.Entry(pricepage)
+        suvdis.grid(row=3, column=1)
+        mycursor.execute("SELECT discountprice FROM Prices WHERE cartype = \'suv\'")
+        suvdis.insert(0, mycursor.fetchone()[0])
+        
+        tk.Label(pricepage, text='Pick-up Price/Day', height=5,width=20,padx=10,pady=5).grid(row=4, column=0)
+        pickupreg = tk.Entry(pricepage)
+        pickupreg.grid(row=5, column=0)
+        mycursor.execute("SELECT price FROM Prices WHERE cartype = \'pickup\'")
+        pickupreg.insert(0, mycursor.fetchone()[0])
+        
+        tk.Label(pricepage, text='Pick-up Discount Price/Day', height=5,width=20,padx=10,pady=5).grid(row=4, column=1)
+        pickupdis = tk.Entry(pricepage)
+        pickupdis.grid(row=5, column=1)
+        mycursor.execute("SELECT discountprice FROM Prices WHERE cartype = \'pickup\'")
+        pickupdis.insert(0, mycursor.fetchone()[0])
+        
+        tk.Label(pricepage, text='Van Price/Day', height=5,width=20,padx=10,pady=5).grid(row=6, column=0)
+        vanreg = tk.Entry(pricepage)
+        vanreg.grid(row=7, column=0)
+        mycursor.execute("SELECT price FROM Prices WHERE cartype = \'van\'")
+        vanreg.insert(0, mycursor.fetchone()[0])
+        
+        tk.Label(pricepage, text='Van Discount Price/Day', height=5,width=20,padx=10,pady=5).grid(row=6, column=1)
+        vandis = tk.Entry(pricepage)
+        vandis.grid(row=7, column=1)
+        mycursor.execute("SELECT discountprice FROM Prices WHERE cartype = \'van\'")
+        vandis.insert(0, mycursor.fetchone()[0])
+        
+        # commit changes of price to database
+        def updateprices():
+            # make sure values are floats
+            try:
+                ser = float(sedanreg.get())
+                sed = float(sedandis.get())
+                sur = float(suvreg.get())
+                sud = float(suvdis.get())
+                pur = float(pickupreg.get())
+                pud = float(pickupdis.get())
+                vnr = float(vanreg.get())
+                vnd = float(vandis.get())
+            # show the error msg
+            except ValueError:
+                print('Update price is invalid!')
+                popupfn('Update price is invalid!\nPlease make sure it is a number.')
+            # update the prices
+            else:
+                sql = "UPDATE Prices SET price = %s, discountprice = %s WHERE cartype = %s;"
+                mycursor.execute(sql, (ser, sed, 'sedan'))
+                mycursor.execute(sql, (sur, sud, 'suv'))
+                mycursor.execute(sql, (pur, pud, 'pickup'))
+                mycursor.execute(sql, (vnr, vnd, 'van'))
+                
+                mydb.commit()
+                
+                print('Prices successfully updated!')
+                popupfn('Prices sucessfully updated!')
+                pricepage.destroy()
+        
+        updatebtn = tk.Button(pricepage, text="Update Prices", command=updateprices, height=5,width=15,padx=10,pady=5)
+        updatebtn.grid(row=8, columnspan=2)
+    
+    browsebtn = tk.Button(johnson, text="Browse Reservations", command=browsepage, height=5,width=15,padx=10,pady=5)
+    browsebtn.grid(row=0, column=0)
+    
+    pricebtn = tk.Button(johnson, text="Change Prices", command=changeprice, height=5,width=15,padx=10,pady=5)
+    pricebtn.grid(row=0, column=1)
+    
 
 
 # LOGIN PAGE
 customerbtn = tk.Button(root, text="Continue as Customer", command=reservepage, height=5,width=15,padx=10,pady=5)
 customerbtn.grid(row=0)
 
-johnsonbtn = tk.Button(root, text="Login as Mr. Johnson", command=browsepage, height=5,width=15,padx=10,pady=5)
+johnsonbtn = tk.Button(root, text="Login as Mr. Johnson", command=johnsonpage, height=5,width=15,padx=10,pady=5)
 johnsonbtn.grid(row=1)
 
 exitbtn = tk.Button(root, text="Exit", command=root.destroy, height=5,width=15,padx=10,pady=5)
